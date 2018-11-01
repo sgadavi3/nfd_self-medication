b6fd361f97942e6961cce42057ffc3e255f0df50
Alec Davies <sgadavi3@gmail.com>
2018-11-01 11:34:20 +0000

    Update code

--- randomForest_XGBoost.R
+++ randomForest_XGBoost.R
@@ -1,57 +1,53 @@
-#packages 
-library(data.table)
+#load required packages
 library(caret)
 library(classInt)
+library(data.table)
 library(ggalluvial)
 library(ggplot2)
-library(ggpubr)
 library(Matrix)
 library(pdp)
 library(randomForest)
+library(usdm)
+library(xgboost)
 
-#data
+#load data
 data(iris)
+#one hot code categorical variables
 iris <- as.data.table(as.matrix(sparse.model.matrix(~.-1, data = iris)))
 
-#reproduce results by setting seed
-set.seed(123)
+#explore correlations (target variable is Petal.Width)
+cor(iris[,1:4])
+#perform vif analysis to check for multicolinearity
+vif(iris[,1:3])
+#select variables from the correlation and vif analysis to include in models
 
-#index data to split 
-trainIndex <- createDataPartition(iris$Sepal.Length, p = .8, list = FALSE, times = 1)
-#split data to train and test 
+#index data to split on
+set.seed(16)
+trainIndex <- createDataPartition(iris$Petal.Width, p = .7, list = FALSE, times = 1)
+#split data into train and test 
 iTrain <- iris[trainIndex,]
 iTest <- iris[-trainIndex,]
 
-#parameter tuning
-tc <- trainControl(method="repeatedcv", number = 10, repeats = 3, allowParallel = T)
+#perform Random Forests 
+Rfit <- randomForest(Petal.Width ~ ., data=iTrain, 
+                     importance=TRUE, 
+                     ntree=500, 
+                     mtry = ncol(iTrain)/3)
 
-#Random Forest
-#parameter grid search (mtry = p/3 for regression)
-mtry <- ncol(iTrain) / 3
-rfgrid <- expand.grid(.mtry = mtry)
-
-#run rf model (replace 'rf' with 'parRF' for parallel application)
-system.time(RfFit <- train(Sepal.Length ~ ., 
-                            data = iTrain, 
-                            method = 'rf', 
-                            trainControl = tc,
-                            verbose = F, 
-                            tuneGrid = rfgrid, 
-                            ntree = 500, 
-                            importance = T,
-                            verboseIter = T))
-
-#XGBoost 
-#parameter grid search 
-xgbGrid <- expand.grid(nrounds = seq(50, 500, 50),
-                       eta = seq(0.075, 0.3, 0.075),
+#perform XGBoost
+#set up 10 fold cross validation for parameter grid search 
+tc <- trainControl(method="repeatedcv", number = 10, repeats = 1, allowParallel = T)
+#set up grid for parameter grid search (low rounds and largers learning rate to reduce time)
+xgbGrid <- expand.grid(nrounds = seq(100, 500, 200),
+                       eta = 0.3,
                        max_depth = seq(4, 6, 4),
                        gamma = 0, 
                        min_child_weight = 1, 
-                       colsample_bytree = 1, 
-                       subsample = 1)
-#run XGBoost model
-xgbFit <- train(Sepal.Length ~ ., 
+                       colsample_bytree = 0.7, 
+                       subsample = 0.7)
+#run parameter tuning
+set.seed(16)
+xgbFit <- train(Petal.Width ~ ., 
                 data = iTrain, 
                 method = 'xgbTree', 
                 trainControl = tc,
@@ -59,35 +55,60 @@ xgbFit <- train(Sepal.Length ~ .,
                 tuneGrid = xgbGrid,
                 importance = T)
 
+#set parameters for final model 
+param_Xfit <- list(booster="gbtree",
+                   silent=0,
+                   eta=0.001,
+                   max_depth=6,
+                   #for proportion use reg:logistic
+                   objective="reg:linear",
+                   eval_metric='rmse',
+                   subsample=0.7)
 
-#model summary 
-RfFit
-xgbFit
+#create watchlist to check performance between train and test (have to convert data to xgb.DMatrix() to use xgboost)
+watchlist <- list(train=xgb.DMatrix(as.matrix(iTrain[,c(1:3,5:7)]), 
+                                       label = iTrain$Petal.Width), 
+                  test=xgb.DMatrix(as.matrix(iTest[,c(1:3,5:7)]), 
+                                       label = iTest$Petal.Width))
+#run cross validation with the eta reduced to find the best number of rounds
+set.seed(16)
+xgb.cv(params = param_Xfit,
+       data = xgb.DMatrix(as.matrix(iTrain[, c(1:3, 5:7)]),
+                          label = iTrain$Petal.Width),
+       watchlist = watchlist,
+       nrounds = 5000,
+       nfold = 10,
+       early_stopping_rounds = 50)
+#run the final xgboost model
+set.seed(16)
+Xfit <-xgb.train(params=param_Xfit,
+                      data=xgb.DMatrix(as.matrix(iTrain[,c(1:3,5:7)]), 
+                                       label = iTrain$Petal.Width),
+                      watchlist = watchlist,
+                      nrounds=5000, importance = T)
 
 #variable importance 
-z <- varImp(RfFit) 
-y <- varImp(xgbFit)
+z <- Rfit$importance
+y <- xgb.importance(colnames(iTest), model = Xfit)
 
+#alluvial plots (extension of Sankey diagrams)
 #RF featimp rank 
-x <- as.data.table(z$importance)
-x$var <- c("Sepal.Width", "Petal.Length", "Petal.Width", "Speciessetosa", "Speciesversicolor", "Speciesvirginica")
-x <- x[order(-x$Overall),]
+x <- as.data.table(z)
+x <- x[order(-x$`%IncMSE`),]
+x$var <- c("Petal.Length", "Speciessetosa", "Speciesversicolor", "Speciesvirginica", 
+           "Sepal.Width", "Sepal.Length")
 x$rank <- c(1:6)
-
 #XGBoost featimp rank 
-w <- as.data.table(y$importance)
-y
-w$var <- c("Petal.Length", "Sepal.Width", "Petal.Width", "Speciesversicolor", "Speciesvirginica", "Speciessetosa")
-w$rank <- c(1:6)
-
-#colours
+w <- as.data.frame(y[,1:2])
+w$rank <- c(1:5)
+w[nrow(w) + 1, 1:3] = list("Speciessetosa", 0, 6)
+#set colours 
 colfunc <- colorRampPalette(c("deepskyblue2","orange","brown1"))
 foo <- classIntervals(w$rank, 3, "quantile")
 w$quants <- cut(w$rank, breaks = foo$brks, labels=as.character(1:3))
 w[is.na(w)] <- 1
-v <- merge(w,x,"var")
-
-#Plot
+v <- merge(w, x, by.x ="Feature", by.y = "var")
+#create plot 
 u <- ggplot(v,
             aes(weight = 1, axis1 = rank.x, axis2 = rank.y)) + 
             geom_alluvium(aes(fill = quants), width = 1/24) +
@@ -101,31 +122,22 @@ u <- ggplot(v,
 u   
 
 #partial dependency plots
-rfPart <- partial(RfFit, 
+set.seed(16)
+rfPart <- partial(Rfit, 
                   var.data = iTrain,
                   pred.var = "Petal.Length",
-                  plot = F)
-xgbPart <- partial(xgbFit, 
-                  var.data = iTrain,
+                  plot = T)
+xgbPart <- partial(Xfit, 
+                  train = as.matrix(iTrain[,c(1:3,5:7)]),
                   pred.var = "Petal.Length",
-                  plot = F)
+                  plot = T)
 
 
-#plot pdp using ggplot2 (replace geom_line with geom_point for points)
-a <- ggplot(rfPart, aes(x = Petal.Length, y = yhat)) +
-    geom_line()
-b <- ggplot(xgbPart, aes(x = Petal.Length, y = yhat)) +
-    geom_line()
-
-ggarrange(a,b,nrow=1,ncol=2)
-
-#test on 30% unseen data
-rfPred <- predict(RfFit, iTest)
-xgbPred <- predict(xgbFit, iTest)
-
-
-#summary metrics 
-rfSum <- postResample(rfPred, iTest$Sepal.Length)
-xgbSum <- postResample(xgbPred, iTest$Sepal.Length)
-rfSum
-xgbSum
+#model summary metrics
+#random forest
+rfpred <- predict(Rfit, iTest[,c(1:3,5:7)])
+postResample(rfpred, iTest$Petal.Width)
+#xgboost
+xgbpred <- predict(Xfit, xgb.DMatrix(as.matrix(iTest[,c(1:3,5:7)]), 
+                                           label = iTest$Petal.Width))
+postResample(xgbpred, iTest$Petal.Width)
